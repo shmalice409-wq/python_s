195df642feddb964f09447172552cc054881ba3f
LiuGuanYi <shmalice409@gmail.com>
2026-06-28 23:51:39 +0800

    线性回归添加注释

--- DL/test.py
+++ DL/test.py
@@ -1,3 +1,12 @@
-import torch
+import random
 
-print(torch.__version__)
+import torch
+features=torch.tensor([1,23,4,5,6,7,8,9])   
+num_examples=len(features)
+indices =list(range(num_examples))
+random.shuffle(indices)  # 样本的读取顺序是随机的
+for i in range(0, num_examples, 4):
+    batch_indices = torch.tensor(
+        indices[i: min(i + 4, num_examples)])
+    print(batch_indices)
+    print(features[batch_indices])
