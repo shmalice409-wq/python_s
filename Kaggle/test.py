31decba0a4bc4f4595d7b1dfc506b21a314d7f1b
LiuGuanYi <shmalice409@gmail.com>
2026-07-10 17:21:58 +0800

    房价预测竞赛第一次ref 54522544

--- Kaggle/test.py
+++ Kaggle/test.py
@@ -1,3 +1,8 @@
+import sys
+import pandas as pd
 import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
+
+print("Python解释器：", sys.executable)
+print("Pandas版本：", pd.__version__)
+print("PyTorch版本：", torch.__version__)
+print("CUDA是否可用：", torch.cuda.is_available())
